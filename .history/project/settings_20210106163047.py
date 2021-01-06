e5b5895190e41257f4cbda20a2ede089bdeb4803
lienusrob <lienus.rob@hotmail.de>
2021-01-06 16:31:01 +0100

    updates for upload

--- .history/project/settings_20210106163047.py
+++ .history/project/settings_20210106163047.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = ['lienusproject.herokuapp.com', '127.0.0.1']
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles"),
+STATIC_ROOT = os.path.join(BASE_DIR, "static"),
 
 # Application definition
 
