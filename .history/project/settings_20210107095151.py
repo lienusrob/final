38fcc83dec51541b5f36c6effe8844029990d005
lienusrob <lienus.rob@hotmail.de>
2021-01-07 09:52:09 +0100

    update

--- .history/project/settings_20210107095151.py
+++ .history/project/settings_20210107095151.py
@@ -34,7 +34,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 # Application definition
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 INSTALLED_APPS = [
     
     'django.contrib.admin',
